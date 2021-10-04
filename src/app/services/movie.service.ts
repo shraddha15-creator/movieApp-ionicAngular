f7c4ce2d22e719df5afb3b277ffac9fc22ef7afb
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2021-10-04 15:21:12 +0530

    initial commit

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export enum SearchType{
+  all = '',
+  movies = 'movies', 
+  series = 'series',
+  episodes = 'episodes'
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  url = "http://www.omdbapi.com/";
+  apiKey = "10dfa746"
+
+  constructor(private http: HttpClient) { }
+
+  searchData(title: string, type: SearchType) : Observable<any>{
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`)
+      .pipe(
+        map(results => {
+          console.log('RAW:', results);
+          return results['Search'];
+        })
+      )
+  }
+
+  getDetails(id){
+    return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`)
+  }
+}
